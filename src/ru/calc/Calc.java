a18277147c03806f682e325d192a07bde6deaa26
alvad89 <alvad89@gmail.com>
2013-03-29 09:44:16 +0600

    create main class and interface

---
+++ src/ru/calc/Calc.java
@@ -0,0 +1,15 @@
+package ru.calc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: a1
+ * Date: 29.03.13
+ * Time: 8:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class Calc {
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/ru/calc/Operation.java
@@ -0,0 +1,15 @@
+package ru.calc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: a1
+ * Date: 29.03.13
+ * Time: 8:28
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Operation {
+    public void sum(double a, double b);
+    public void multiply(double a, double b);
+    public void sub(double a, double b);
+    public void div(double a, double b);
+}
