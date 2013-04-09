d75621dec361eedab884bfde3d949801504095f3
alvad89 <alvad89@gmail.com>
2013-04-09 17:41:54 +0600

    nevest chto

--- src/ru/calc/OperationImpl.java
+++ src/ru/calc/OperationImpl.java
@@ -5,10 +5,7 @@ package ru.calc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,20 +17,72 @@ import java.util.regex.Pattern;
  * Time: 8:56
  * To change this template use File | Settings | File Templates.
  */
-public class OperationImpl implements Operation{
-    public void operat(){
+public class OperationImpl{
+    public void operat(String[] args){
         System.out.println("Enter expression:");
         //BufferedReader inp = new BufferedReader(new InputStreamReader(System.in));
-        String inputs = "- 2 + 3 * ( 4 - 1 )";
-        Scanner in = new Scanner(inputs);
+        //String inputs = "- 2 + 3 * ( 4 - 1 )";
+       // Scanner in = new Scanner(inputs);
         String all = null;
-        Pattern pat = Pattern.compile("\\s*([-+]?\\d+\\.?\\d*(e[-+]?)?\\d*\\s*)(.*)", Pattern.CASE_INSENSITIVE);
-        System.out.println(in.next(pat));
-        Pattern pt = Pattern.compile("\\s*\\(\\s*(.*)\\s*\\)\\s*([^)]*)$", Pattern.CASE_INSENSITIVE);
-        System.out.println(in.next(pt));
+        LinkedList<String> st = new LinkedList<String>();
 
-        in.close();
-        List<Double> expr = new ArrayList<Double>();
+        for (String arg : args) {
+            System.out.println(arg);
+            st.add(arg);
+
+        }
+        List<Double> variebl = new ArrayList<Double>();
+        Pattern three = Pattern.compile("\\s*([+-]?\\d+\\.?\\d*(e[+-]?)?\\d*\\s*)", Pattern.CASE_INSENSITIVE);
+        //Pattern three = Pattern.compile("\\s*\\(\\s*(.*)\\s*\\)\\s([^)]*)$", Pattern.CASE_INSENSITIVE);
+        //Pattern three = Pattern.compile("^([^ ]*)( )(.*)$",Pattern.CASE_INSENSITIVE);
+        Matcher matcher = three.matcher(st.get(0));
+        System.out.println(matcher.groupCount());
+        while (matcher.find()){
+            int start = matcher.start();
+            int end=matcher.end();
+            String s = st.get(0).substring(start, end);
+            //System.out.println(s);
+            variebl.add(Double.valueOf(s));
+        }
+
+        int previous =0;
+        System.out.println(variebl);
+        LinkedList<String> expr = new LinkedList<String>();
+        char[] chars = st.get(0).toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            Character aChar = chars[i];
+            expr.add(aChar.toString());
+            Matcher matcherNumb = three.matcher(aChar.toString());
+            if (matcherNumb.matches()){
+              //  System.out.println("trueee");
+                if ((i-previous)>1){
+                    previous=i;
+                }
+            }
+
+
+            switch (aChar) {
+                case '+':
+                //    System.out.println("+");
+                    break;
+                case '-':
+                //    System.out.println("-");
+                    break;
+                case '*':
+                //    System.out.println("*");
+                    break;
+                case '/':
+                //    System.out.println("-");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        Iterator<String> iter = expr.iterator();
+
+
+      //  List<Double> expr = new ArrayList<Double>();
         List<Character> action = new ArrayList<Character>();
         exprOne = 0;
 
@@ -113,24 +162,21 @@ public class OperationImpl implements Operation{
 
     }
     private double exprOne;
-
-    @Override
-    public Double sum(double a, double b) {
-        return a+b;
+    public boolean isOperand(char s){
+        switch (s){
+            case '0':return true;
+            case '1':return true;
+            case '2':return true;
+            case '3':return true;
+            case '4':return true;
+            case '5':return true;
+            case '6':return true;
+            case '7':return true;
+            case '8':return true;
+            case '9':return true;
+        }
+        return false;
     }
 
-    @Override
-    public Double multiply(double a, double b) {
-        return a*b;
-    }
-
-    @Override
-    public Double sub(double a, double b) {
-        return a-b;
-    }
-
-    @Override
-    public Double div(double a, double b) {
-        return a/b;
-    }
+ 
 }
---
+++ src/ru/calc/Sympol.java
@@ -0,0 +1,10 @@
+package ru.calc;
+
+public enum Sympol {
+    OPERATOR,
+    OPERAND;
+    public boolean isOperand(Character a){
+
+        return false;
+    }
+}
